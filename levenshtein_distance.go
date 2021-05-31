a4df59450475c6cf8906c8016fcef39b25f28948
den3tsou <den3tsou@gmail.com>
2021-05-31 20:26:28 +1000

    more efficient

--- levenshtein_distance.go
+++ levenshtein_distance.go
@@ -1,38 +1,35 @@
 package ed
 
 func LevenshteinDistance(a, b string) int {
-	al := len(a) + 1
-	bl := len(b) + 1
-	d := make([][]int, al)
-	for i := range d {
-		d[i] = make([]int, bl)
+	al := len(a)
+	bl := len(b)
+	r0 := make([]int, bl+1)
+	r1 := make([]int, bl+1)
+
+	for i := 0; i < bl+1; i++ {
+		r0[i] = i
 	}
 
-	for i := 1; i < al; i++ {
-		d[i][0] = i
-	}
+	for i := 0; i < al; i++ {
+		r1[0] = i + 1
 
-	for j := 1; j < bl; j++ {
-		d[0][j] = j
-	}
-
-	var cost int
-	for j := 1; j < bl; j++ {
-		for i := 1; i < al; i++ {
-			if a[i-1] == b[j-1] {
-				cost = 0
-			} else {
-				cost = 1
+		for j := 0; j < bl; j++ {
+			deletionCost := r0[j+1] + 1
+			insertionCost := r1[j] + 1
+			substitutionCost := r0[j]
+			if a[i] != b[j] {
+				substitutionCost += 1
 			}
-			d[i][j] = min(
-				d[i-1][j]+1,
-				d[i][j-1]+1,
-				d[i-1][j-1]+cost,
-			)
+
+			r1[j+1] = min(deletionCost, insertionCost, substitutionCost)
+		}
+
+		for i := 0; i < bl+1; i++ {
+			r0[i] = r1[i]
 		}
 	}
 
-	return d[al-1][bl-1]
+	return r0[bl]
 }
 
 func min(a, b, c int) int {
